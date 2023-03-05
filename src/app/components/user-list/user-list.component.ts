6a41d0ac3926bc90702b9835414035597ff967b0
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 16:13:16 +0400

    feat: add business-logic in component

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
+import { Observable, Subscription } from 'rxjs';
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnDestroy {
+  users: any;
+  subscription!: Subscription;
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.subscription = this.apiService
+      .getUsers()
+      .subscribe((users) => (this.users = users));
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
