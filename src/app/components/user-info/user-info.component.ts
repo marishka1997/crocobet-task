1009e2139a3ec6f86ff8d4b2038d0151d4230ce0
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 16:48:50 +0400

    feat: add business-logic in component

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
 @Component({
   selector: 'app-user-info',
   templateUrl: './user-info.component.html',
   styleUrls: ['./user-info.component.scss']
 })
-export class UserInfoComponent {
+export class UserInfoComponent implements OnInit {
+  id!: number;
+  user: any;
+  posts: any;
+  userId: any;
 
+  constructor(
+    private apiService: ApiService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['usersId'];
+
+    this.apiService.find(this.id).subscribe((user: any) => {
+      this.user = user;
+    });
+  }
+
+  back() {
+    this.router.navigate(['/']);
+  }
 }
