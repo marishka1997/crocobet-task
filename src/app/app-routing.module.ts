ce66622a8e9f2245e6ca110a025c09822d489668
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 16:18:27 +0400

    refactor: add route for us UserListComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { UserListComponent, } from './components/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserListComponent,
+  },
+  // {
+  //   path: 'user-info/:usersId',
+  //   component: UserInfoComponent
+  // },
+  // {
+  //   path: 'user-info/:usersId/posts',
+  //   component: UserPostsComponent
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
