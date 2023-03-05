3744df9e02cb9c3a530b364d8e3e23455a4ce0ec
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 17:02:22 +0400

    feat: add business-logic in component

--- src/app/components/user-posts/user-posts.component.ts
+++ src/app/components/user-posts/user-posts.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-user-posts',
   templateUrl: './user-posts.component.html',
   styleUrls: ['./user-posts.component.scss']
 })
-export class UserPostsComponent {
+export class UserPostsComponent implements OnInit, OnDestroy {
+  userId:any
+  posts:any
+  users:any
+  id:any
 
+  subscription!: Subscription;
+
+    constructor(private apiService: ApiService, private router: Router,  private route: ActivatedRoute,) { }
+
+    ngOnInit(): void {
+
+      this.userId = this.route.snapshot.params['usersId'];
+
+     this.subscription = this.apiService.findPost(this.userId).subscribe((Posts: any)=>{
+        this.posts = Posts
+      });
+    }
+
+    list(){
+      this.router.navigate(['/']);
+    }
+
+    ngOnDestroy() {
+      this.subscription.unsubscribe();
+    }
 }
