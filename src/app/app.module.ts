6c9b7083af4377c9a477b7bd3c7f1d9e125da010
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 14:28:05 +0400

    feat: add 4 components and 1 service, update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserInfoComponent } from './components/user-info/user-info.component';
+import { UserPostsComponent } from './components/user-posts/user-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserListComponent,
+    UserInfoComponent,
+    UserPostsComponent
   ],
   imports: [
     BrowserModule,
