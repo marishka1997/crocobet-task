91fe362fd3223de130b5de3a0da208a2ce431eec
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 16:09:45 +0400

    refactor: add RouterModule and HttpClientModule in imports array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { UserInfoComponent } from './components/user-info/user-info.component';
 import { UserPostsComponent } from './components/user-posts/user-posts.component';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { UserPostsComponent } from './components/user-posts/user-posts.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
