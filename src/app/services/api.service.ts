b4dd5dc28176f3562f520a5709aff67a61afccb8
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 16:06:36 +0400

    feat: add observables and methods using http-client

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getUsers(): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/users`);
+  }
+  getPosts(): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+  }
+  find(id:number): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+  }
+  findPost(userId:any): Observable<any> {
+    return this.http.get(`https://jsonplaceholder.typicode.com/posts/?userId=${userId}`)
+  }
 }
