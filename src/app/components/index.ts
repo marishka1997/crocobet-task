46c4c6efeae2604262be0f2de8eeb0181bd6c871
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-05 17:01:56 +0400

    fix: add imports in index.ts

---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export * from './header/header.component';
+export * from './user-info/user-info.component';
+export * from './user-list/user-list.component';
+export * from './user-posts/user-posts.component';
