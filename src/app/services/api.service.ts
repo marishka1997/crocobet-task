12969c4290a201b6c59db860bfe9d7309b97e7ff
Mariam Gasparian <m.gasparian11@gmail.com>
2023-03-13 19:54:39 +0400

    fix: update methods and import models

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,23 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from "@angular/common/http";
 import { Observable } from 'rxjs';
-
+import { User } from './models/user.model';
+import { Post } from './models/posts.model';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
   constructor(private http: HttpClient) { }
-  getUsers(): Observable<any> {
-    return this.http.get(`https://jsonplaceholder.typicode.com/users`);
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`https://jsonplaceholder.typicode.com/users`);
   }
-  getPosts(): Observable<any> {
-    return this.http.get(`https://jsonplaceholder.typicode.com/posts`);
+  getPosts(): Observable<Post> {
+    return this.http.get<Post>(`https://jsonplaceholder.typicode.com/posts`);
   }
-  find(id:number): Observable<any> {
-    return this.http.get(`https://jsonplaceholder.typicode.com/users/${id}`)
+  find(id: number): Observable<User> {
+    return this.http.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`);
   }
-  findPost(userId:any): Observable<any> {
-    return this.http.get(`https://jsonplaceholder.typicode.com/posts/?userId=${userId}`)
+  findPost(userId:any): Observable<Post[]> {
+    return this.http.get<Post[]>(`https://jsonplaceholder.typicode.com/posts/?userId=${userId}`)
   }
 }
